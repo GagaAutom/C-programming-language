29f6a9678a5a8ae2708442fc8c185550397d4762
GagaAutom <1049340665@qq.com>
2020-02-24 09:08:08 +0800

    公约数问题

---
+++ common divisor/common divisor/text1.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int num1, num2, temp, i;
+	printf("please input number:\n");
+	scanf("%d %d", &num1, &num2);
+	if (num1 < num2)
+	{
+		temp = num1;
+		num1 = num2;
+		num2 = temp;
+	}
+	temp = 0;
+	for (i = 1; i < num1; i++)
+	{
+		if (num1%i == 0 && num2%i == 0)
+		{
+			temp = i;
+		}
+	}
+	printf("the max common divisor : %d\n", temp);
+	system("pause");
+	return 0;
+}
