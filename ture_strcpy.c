0eea1589ea1dd155b28a930ae7525a41cf07d041
GagaAutom <1049340665@qq.com>
2020-04-01 20:58:14 +0800

    真实版本strcpy

---
+++ ture_strcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <Windows.h>
+#include<assert.h>
+#pragma warning(disable:4996)
+
+char *my_strcpy(char *dest, const char*src)
+{
+	char *ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+
+	while ((*dest++ = *src++))
+	{
+		;
+	}
+	return ret;
+}
+int main()
+{
+	char str1[20];
+	strcpy(str1, "To be ");
+	puts(str1);
+	system("pause");
+	return 0;
+}
