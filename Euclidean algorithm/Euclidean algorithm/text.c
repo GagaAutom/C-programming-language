58331f761199d5d93d8afca19ab7bc619c1b06cb
GagaAutom <1049340665@qq.com>
2020-06-06 17:55:09 +0800

    辗转相除法求公约数

--- Euclidean algorithm/Euclidean algorithm/text.c
+++ Euclidean algorithm/Euclidean algorithm/text.c
@@ -7,12 +7,12 @@ int main()
 	int num1, num2, temp, i;
 	printf("please input number:\n");
 	scanf("%d %d", &num1, &num2);
-	if (num1 < num2)
-	{
-		temp = num1;
-		num1 = num2;
-		num2 = temp;
-	}
+	//if (num1 < num2)
+	//{
+	//	temp = num1;
+	//	num1 = num2;
+	//	num2 = temp;
+	//}
 	temp = 0;
 	while (1) {
 		temp = num1 % num2;
